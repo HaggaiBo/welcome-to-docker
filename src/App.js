f2ec5a8f4327d0866eae8216ed05ea7b126ad532
Haggai Bonni <hagiybonni@gmail.com>
2023-10-22 07:40:31 +0300

    first change

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ const App = () => {
           <a target="_blank" href={"https://twitter.com/intent/tweet?text=" + shareMessage + "&url=" + shareLink} class="fa fa-twitter" rel="noopener noreferrer"> </a>
           <a target="_blank" href={"https://www.linkedin.com/sharing/share-offsite/?url=" + shareLink} class="fa fa-linkedin" rel="noopener noreferrer"> </a>
           <a target="_blank" href={"https://reddit.com/submit?title=" + shareMessage + "&url=" + shareLink} class="fa fa-reddit" rel="noopener noreferrer"> </a>
+          <a target="_blank" href={"https://reddit.com/submit?title=" + shareMessage + "&url=" + shareLink} class="fa fa-reddit" rel="noopener noreferrer"> </a>
         </div>
       </header>
     </div>
